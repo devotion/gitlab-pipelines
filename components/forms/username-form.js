3955cf741c769a0bf399afad8236204e8df07742
Milos Dzeletovic <milos.dzeletovic@consulteer.com>
2019-12-20 00:21:52 +0100

    Edit home page

---
+++ components/forms/username-form.js
@@ -0,0 +1,36 @@
+import { Formik, Form } from 'formik'
+import * as Yup from 'yup'
+
+import TextInput from '../form/text-input'
+
+import './username-form.scss'
+
+const validationSchema = Yup.object().shape({
+  username: Yup.string()
+})
+
+function UsernameForm() {
+  function onSubmit() {
+    console.log('submitted')
+  }
+
+  return (
+    <Formik
+      onSubmit={onSubmit}
+      initialValues={{
+        username: ''
+      }}
+      validationSchema={validationSchema}
+    >
+      <Form className="username-form" autoComplete="off">
+        <TextInput
+          placeholder="GitLab username"
+          id="username"
+          name="username"
+        />
+      </Form>
+    </Formik>
+  )
+}
+
+export default UsernameForm
--- pages/index.js
+++ pages/index.js
@@ -1,8 +1,9 @@
-import { useEffect, useContext } from 'react'
+import { useEffect, useContext, useState } from 'react'
 import Router from 'next/router'
 
 import Layout from '../components/layout'
 import LoadingPage from '../components/loading/loading-page'
+import UsernameForm from '../components/forms/username-form'
 import { AuthContext } from '../contexts/auth'
 
 import './index.scss'
@@ -12,13 +13,15 @@ function Home() {
     credentials: { token }
   } = useContext(AuthContext)
 
+  const [notificationsDefault, setNotificationsDefault] = useState(false)
+
   useEffect(() => {
     if (!token) {
       Router.push('/login')
     }
 
     if (Notification.permission === 'default') {
-      Notification.requestPermission()
+      setNotificationsDefault(true)
     }
   }, [token])
 
@@ -33,8 +36,48 @@ function Home() {
   return (
     <Layout title="Home">
       <div className="home">
-        <h2>Welcome to GitLab Pipelines.</h2>
+        <h1>Welcome to GitLab Pipelines</h1>
         <p>Choose a project and let&apos;s start</p>
+
+        <div className="home__feature">
+          <h2>Track your pipelines</h2>
+          <p>
+            This is an open source project meant to make it easier for you to
+            track your GitLab pipelines.
+          </p>
+        </div>
+
+        {notificationsDefault && (
+          <div className="home__feature">
+            <h2>We have browser notifications</h2>
+            <p>
+              App can send you notifications when some of your monitored
+              pipelines pass or fail. Since it is a browser feature, you need to
+              enable by clicking here and then choosing `allow`
+            </p>
+            <p>
+              You can edit to receive notifications for pipelines you like
+              tracked. By default, you will get them for master branch and for a
+              chosen username.
+            </p>
+            <button
+              className="button button-full"
+              onClick={() => {
+                Notification.requestPermission()
+              }}
+            >
+              Enable notifications
+            </button>
+          </div>
+        )}
+        <div className="home__feature">
+          <h2>Add your username</h2>
+          <p>
+            You can add your gitlab username to enable notifications by default
+            for the pipelines that you create
+          </p>
+          <UsernameForm />
+        </div>
       </div>
     </Layout>
   )
